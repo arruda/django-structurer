bcb6a64aa9c7a81fdeba18f946c81b5cc2575a2d
Felipe Pontes <arruda@uniriotec.com>
2011-10-10 19:40:45 -0300

    now it tries to load the file passed as second argument, but if its not in that folder it tries it on the enviroment folder for djstruct

--- django_structurer/bin/djstruct.py
+++ django_structurer/bin/djstruct.py
@@ -4,13 +4,35 @@ import sys
 import os
 import django_structurer
 from django_structurer.structurer import project_starter
+from django_structurer.snippets_readers import CUSTOM_SNIPPETS
+
+DEFAULT_PROJECT = os.path.join(django_structurer.__path__[0], 'default_structure.yaml')
+
+def check_file(file_name, path):
+    try:        
+        file_path = os.path.join(path,file_name)
+        yaml_file = file(file_path, 'r')
+        return yaml_file
+    except:
+        return None
+
+def tries_different_paths_for_file(file_name):    
+    yaml_file = check_file(file_name, "./")
+    if yaml_file == None:            
+        yaml_file = check_file(file_name, CUSTOM_SNIPPETS)
+        if yaml_file == None:
+            yaml_file = file(DEFAULT_PROJECT,'r')
+
+    return yaml_file
 
 if __name__ == "__main__":
     project_name = sys.argv[1]
-    try:
-        yaml_project = file(sys.argv[2], 'r')
-    except:
-        default_proj = os.path.join(django_structurer.__path__[0], 'default_structure.yaml')
-        yaml_project = file(default_proj,'r')
+    yaml_project = DEFAULT_PROJECT
+    try:        
+        yaml_project = sys.argv[2]
+    except IndexError:
+        pass    
 
-    project_starter(project_name, yaml_project)
+    yaml_file = tries_different_paths_for_file(yaml_project)
+    project_starter(project_name, yaml_file)
+
