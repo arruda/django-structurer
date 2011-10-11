3df4723bfae5b20990f7dd60c3397c8d2a63cb87
Felipe Pontes <arruda@uniriotec.com>
2011-10-11 15:26:30 -0300

    starting dump of snippets

--- django_structurer/snippets_readers.py
+++ django_structurer/snippets_readers.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 import os
 
+SNIPPETS_TERM = ".snippets"
 SNIPPETS_FOLDER = "snippets"
 DEFAULT_PATH = os.path.join(SNIPPETS_FOLDER,'default')
 #DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'snippets','default')
@@ -19,10 +20,14 @@ class Snippets(object):
     def mount_key(cls,full_path,file_path):
         """Generates a key for a snippet by removing the first part of full_path that contains
         the string 'snippets.' from the path.
+        EX:
+        The file: snippets/default/others/headers.snippets you have a key:
+        'default.others.headers'
         """
         snpt_path_fix = os.path.join(SNIPPETS_FOLDER, "")
-        init = full_path.find(snpt_path_fix)+snpt_path_fix.__len__()        
-        alter_path = os.path.join(full_path,file_path).replace(full_path[:init],"")
+        init = full_path.find(snpt_path_fix)+snpt_path_fix.__len__()      
+        alter_file_path = file_path.replace(SNIPPETS_TERM,"")  
+        alter_path = os.path.join(full_path,alter_file_path).replace(full_path[:init],"")
         key = alter_path.replace("/",".")
         return key
 
@@ -32,7 +37,7 @@ class Snippets(object):
         full_path = os.path.join(os.path.dirname(__file__), folder)
         for d, subdirs, files in os.walk(full_path):             
             for f in files:
-                if not f.endswith('.snippets'):
+                if not f.endswith(SNIPPETS_TERM):
                     # Ignore .pyc, .pyo, .py.class etc, as they cause various
                     # breakages.
                     continue
@@ -40,8 +45,9 @@ class Snippets(object):
                 file_path = os.path.join(d, f)   
                 key = Snippets.mount_key(d,f)             
                 print key
+                snpt_dict = {'file':file_path, 'txt': None}
                 snippet = open(file_path,'r')
-                self.snippets[key] = snippet.read()
+                self.snippets[key]['txt'] = snippet.read()
                 snippet.close()
                 
 
@@ -53,10 +59,12 @@ class Snippets(object):
             self._load_snippets(DJSTRUCT_HOME)
 
 
-    def dump_snoppets(self):
-        """Dump the snippets from the folder to where the user is using the command.
+    def dump_snippets(self,destination="./"):
+        """Dump the snippets from the user folder, and the default(if True) 
+           to where the user is using the command.
            It actualy copies the default folder(from snippets) into where the user whants.
         """
-        pass
-
-
+        self.load_snippets()
+        for key in self.snippets:
+            old_path = self.snippets[key]['file']
+            print old_path
