c76b1bb5b27e41ad4cbb8bde1c7e6d812fe4c398
Felipe Pontes <arruda@uniriotec.com>
2011-10-11 00:01:09 -0300

    new setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ for dirpath, dirnames, filenames in os.walk(data_folder):
 
 setup(
     name = "django_structurer",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Felipe Arruda Pontes",
     author_email = "contato@arruda.blog.br",
     description = ("This app allow you to create your own custom django project structure, and then use it as you please."),
