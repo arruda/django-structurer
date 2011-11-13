d05d9f7bcd15643ac4aa3dd1fa26a1012a3e1576
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2011-11-13 16:26:50 -0200

    doing the start app stuff, need to make a app_starter.py file in the manage.py file folder, and create a app_structure.yaml in the same path with the specifications for that project

--- django_structurer/structurer.py
+++ django_structurer/structurer.py
@@ -7,15 +7,21 @@ from snippets_readers import Snippets
 import sys
 
 snpt = Snippets()
+manage_file_folder=''
 
-def make_project_structure(archive,root, project_name):
-    if archive['name'] == "$project_name":
-        archive['name'] = project_name
-
-    if archive['var'] == "$create_with_app":
-        return
+def make_structure(archive,root, name, is_project=True):
+    if archive['name'] == "$project_name" or archive['name'] == "$app_name":
+        archive['name'] = name
 
     archive_path = os.path.join(root,archive['name'])
+
+    if archive['var'] is '$source':
+        manage_file_folder=archive_path
+
+    if archive['var'] == "$create_with_app" and is_project:
+        create_app_struct_file(archive)
+        return
+
 
     arc_archives = archive.get('archives',None)
     #check if is a folder or a file
@@ -38,7 +44,35 @@ def make_project_structure(archive,root, project_name):
                 archive_file.write(snippet_txt)
     
         archive_file.close()       
-    
+
+def find_app_folder(archive,root):
+    """Returns the dict that represents the app_folder
+    passing the original archive and root dir
+    """    
+    if archive['name'] == "$project_name" or archive['name'] == "$app_name":
+        archive['name'] = name
+
+    archive_path = os.path.join(root,archive['name'])
+    if archive.get('var', None) == '$create_with_app':
+        return archive, archive_path
+
+    for sub_ar in archive.get('archives',None):
+        dct,path = find_app_folder(sub_ar,archive_path)
+        if dct and path !=None
+            return dct,path
+
+    return None
+
+def app_starter(app_name,yaml_app):
+    """
+    Creates a new app struture in the folder that the user is in,
+    using the yaml file that discribes how it should do it.
+    """
+    snpt.load_snippets()
+    archives = yaml.load(yaml_project)
+    find_app_folder(archives,)
+    make_project_structure(archives,"./",project_name)
+
 def project_starter(project_name,yaml_project):
     """
     Creates a new Django project struture in the folder that the user is in,
