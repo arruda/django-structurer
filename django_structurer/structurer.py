3508e5235067516b726a5cf3e7cbb13b56887043
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2011-11-14 13:35:25 -0200

    doing the app starter, should be working, need to test

--- django_structurer/structurer.py
+++ django_structurer/structurer.py
@@ -7,7 +7,8 @@ from snippets_readers import Snippets
 import sys
 
 snpt = Snippets()
-manage_file_folder=''
+manage_file_folder=None
+app_folder=None
 
 def make_structure(archive,root, name, is_project=True):
     if archive['name'] == "$project_name" or archive['name'] == "$app_name":
@@ -18,9 +19,12 @@ def make_structure(archive,root, name, is_project=True):
     if archive['var'] is '$source':
         manage_file_folder=archive_path
 
-    if archive['var'] == "$create_with_app" and is_project:
-        create_app_struct_file(archive)
-        return
+    if archive['var'] == "$create_with_app":
+        app_folder=archive_path
+        if is_project:
+            change_proj_name(archive,name)
+            create_app_struct_file(archive)
+            return
 
 
     arc_archives = archive.get('archives',None)
@@ -43,25 +47,44 @@ def make_structure(archive,root, name, is_project=True):
             if snippet_txt != None:
                 archive_file.write(snippet_txt)
     
-        archive_file.close()       
+        archive_file.close()  
+     
+def change_proj_name(archive,project_name):
+    "Change any $project_name for the project name"
+    if archive.get('name',None) is None:
+        return
+    if archive['name'] == "$project_name":
+        archive['name'] = project_name
+    change_proj_name(archive['archives'],project_name)
 
-def find_app_folder(archive,root):
-    """Returns the dict that represents the app_folder
-    passing the original archive and root dir
-    """    
-    if archive['name'] == "$project_name" or archive['name'] == "$app_name":
-        archive['name'] = name
+def create_app_struct_file(archive):
+    """Creates a yaml file that contains the info for a given project
+    that knows how an app should be.
+    """
+    if manage_file_folder != None and app_folder != None:
+        app_strucure = os.path.join(manage_file_folder,'app_structure.yaml')
+        app_strucure = open(app_strucure, "w")
+        yaml.dump(archive,app_strucure)
+        app_strucure.close()
 
-    archive_path = os.path.join(root,archive['name'])
-    if archive.get('var', None) == '$create_with_app':
-        return archive, archive_path
 
-    for sub_ar in archive.get('archives',None):
-        dct,path = find_app_folder(sub_ar,archive_path)
-        if dct and path !=None
-            return dct,path
+#def find_app_folder(archive,root):
+#    """Returns the dict that represents the app_folder
+#    passing the original archive and root dir
+#    """    
+#    if archive['name'] == "$project_name" or archive['name'] == "$app_name":
+#        archive['name'] = name
 
-    return None
+#    archive_path = os.path.join(root,archive['name'])
+#    if archive.get('var', None) == '$create_with_app':
+#        return archive, archive_path
+
+#    for sub_ar in archive.get('archives',None):
+#        dct,path = find_app_folder(sub_ar,archive_path)
+#        if dct and path !=None
+#            return dct,path
+
+#    return None
 
 def app_starter(app_name,yaml_app):
     """
@@ -69,9 +92,9 @@ def app_starter(app_name,yaml_app):
     using the yaml file that discribes how it should do it.
     """
     snpt.load_snippets()
-    archives = yaml.load(yaml_project)
-    find_app_folder(archives,)
-    make_project_structure(archives,"./",project_name)
+    archives = yaml.load(yaml_app)
+    find_app_folder(archives,"./")
+    make_project_structure(archives,"./",app_name,False)
 
 def project_starter(project_name,yaml_project):
     """
@@ -80,7 +103,7 @@ def project_starter(project_name,yaml_project):
     """
     snpt.load_snippets()
     archives = yaml.load(yaml_project)
-    make_project_structure(archives,"./",project_name)
+    make_structure(archives,"./",project_name,True)
 
 #    
 #if __name__ == "__main__":
